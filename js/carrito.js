05a6155e3d65fca933a915186d26489c32d4f38d
Kenat <ks.comdig.pilares@gmail.com>
2020-12-02 23:35:20 -0600

    Version 2

--- js/carrito.js
+++ js/carrito.js
@@ -1,7 +1,12 @@
+
+// Aquí estamos llamando a las etiquetas de los nombres dentro de las comillas dobles"
 const productos = document.getElementById("muestra-productos");
 const contenedor = document.getElementById("muestra-carrito");
-const decisiones = document.getElementById("eventos-carrito")
+const decisiones = document.getElementById("eventos-carrito");
 
+console.log(contenedor.childElementCount)
+
+// Con la función addEventListener("click", ) vamos a escuchar cualquier click que se ejecute dentro de la etiqueta
 productos.addEventListener('click', (e) => { comprarProducto(e) });
 contenedor.addEventListener('click', (e) => {eliminar_producto(e)});
 decisiones.addEventListener('click', (e) => { decision_carrito(e) });
@@ -10,7 +15,12 @@ decisiones.addEventListener('click', (e) => { decision_carrito(e) });
 
 function comprarProducto(e) {
     e.preventDefault();
-    //Delegado para agregar al carrito
+    // Lista = [1,2,3]
+    // Lista[0]  devuelve 1
+    // evento = e
+    // e.target  Estamos identificando que botón está siendo presionado
+    // e.target.classList Estamos creando una lista de las palabras del nombre de nuestra clase
+    
     if (e.target.classList.contains('btn')) {
         const producto = e.target.parentElement.parentElement;
         //Enviamos el producto seleccionado para tomar sus datos
@@ -20,25 +30,28 @@ function comprarProducto(e) {
     }
 }
 
+
 function obtener_datos(producto) {
+    // Sencillamente estamos guardando la ruta, nombre, precio y el id del celular
     const ruta_img = producto.querySelector('img').src;
-    //const precio = producto.querySelector('.producto h1').textContent;
     const nombre = producto.querySelector('.producto h4').textContent
     const precio = Number(producto.querySelector('span').textContent)
     const id = producto.querySelector('button').getAttribute("id");
-
+    
 
     agregar_a_contenedor(nombre, precio, ruta_img, id);
 
 
 }
-//dropdown-menu
+
 function agregar_a_contenedor(name, price, image, id) {
-    const id_cantidad = id + "-cantidad";
+    const id_cantidad = id + "-cantidad"; 
     const cantidad = 1;
 
     const nombres_celulares = contenedor.getElementsByClassName("producto nombre celular");
+    //console.log(nombres_celulares);
 
+    
     for(let i = 0; i < nombres_celulares.length; i++){
         if(nombres_celulares[i].textContent === name){
             var new_etiqueta = document.getElementById(id_cantidad);
@@ -63,12 +76,12 @@ function agregar_a_contenedor(name, price, image, id) {
     </a>
     `;
 
-
     contenedor.prepend(etiqueta);
+
     
 
-}
 
+}
 
 
 
@@ -77,20 +90,23 @@ function decision_carrito(e) {
     e.preventDefault();
     if (e.target.id === "btn-comprar") {
         alert("Procedimiento de pago.")
+
     } else if (e.target.id === "btn-limpiar") {
         vaciar_carrito();
         actualizar_precio();
     } else {
-        console.log("VALIO MADRE");
+        console.log("Algo paso mal");
     }
 
 }
+
 
 function vaciar_carrito(){
     while(contenedor.childElementCount > 2){
         contenedor.removeChild(contenedor.firstChild);
     }
 }
+
 
 function eliminar_producto(e){
     if(e.target.classList.contains('btn-danger')){
@@ -111,12 +127,13 @@ function actualizar_precio(){
     for(var i = 0; i < objeto_carro.length; i++){
         const temporal = Number(objeto_carro[i].textContent)
         const cant_tmp = Number(objeto_carro_cantidad[i].textContent)
-        
+
         total = total + temporal*cant_tmp;
     }
     escribir_precio(total);
     
 }
+
 
 function escribir_precio(precio){
     document.getElementById("idprecio-total").innerHTML = precio;
